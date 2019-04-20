8cfaa6bf599d8df5e3f1b63a31af69e38ab8bd77
Dennis Sauve <dengsauve@yahoo.com>
2019-04-20 12:16:49 -0700

    Sticky Edit Field
    old() has a second parameter that takes a default value. Love it.

--- resources/views/patient/edit.blade.php
+++ resources/views/patient/edit.blade.php
@@ -38,47 +38,47 @@
                             <div class="row">
                                 <div class='form-group col-lg-4'>
                                     <label for='first_name'>first name</label>
-                                    <input type='text' class='form-control' name='first_name' value="{{ $patient->first_name }}"/>
+                                    <input type='text' class='form-control' name='first_name' value="{{ old('first_name', $patient->first_name) }}"/>
                                 </div>
 
                                 <div class='form-group col-lg-4'>
                                     <label for=''>middle name</label>
-                                    <input type='text' class='form-control' name='middle_name' value='{{ $patient->middle_name }}'/>
+                                    <input type='text' class='form-control' name='middle_name' value="{{ old('middle_name', $patient->middle_name) }}"/>
                                 </div>
 
                                 <div class='form-group col-lg-4'>
                                     <label for='last_name'>last name</label>
-                                    <input type='text' class='form-control' name='last_name' value='{{ $patient->last_name }}'/>
+                                    <input type='text' class='form-control' name='last_name' value="{{ old('last_name', $patient->last_name) }}"/>
                                 </div>
                             </div>
 
                             <div class='row'>
                                 <div class='form-group col-lg-6'>
                                     <label for='email'>email address</label>
-                                    <input type='email' class='form-control' name='email' value='{{ $patient->email }}'/>
+                                    <input type='email' class='form-control' name='email' value="{{ old('email', $patient->email) }}"/>
                                 </div>
 
                                 <div class='form-group col-lg-6'>
                                     <label for='phone_number'>phone number</label>
-                                    <input type='tel' class='form-control' name='phone_number' value='{{ $patient->phone_number }}'/>
+                                    <input type='tel' class='form-control' name='phone_number' value="{{ old('phone_number', $patient->phone_number) }}"/>
                                 </div>
                             </div>
 
                             <div class='row'>
                                 <div class='form-group col-lg-6'>
                                     <label for='emergency_contact_name'>emergency contact name</label>
-                                    <input type='text' class='form-control' name='emergency_contact_name' value='{{ $patient->emergency_contact_name }}'/>
+                                    <input type='text' class='form-control' name='emergency_contact_name' value="{{ old('emergency_contact_name', $patient->emergency_contact_name) }}"/>
                                 </div>
 
                                 <div class='form-group col-lg-6'>
                                     <label for='emergency_contact_phone_number'>emergency contact phone number</label>
-                                    <input type='tel' class='form-control' name='emergency_contact_phone_number' value='{{ $patient->emergency_contact_phone_number }}'/>
+                                    <input type='tel' class='form-control' name='emergency_contact_phone_number' value="{{ old('emergency_contact_phone_number', $patient->emergency_contact_phone_number) }}"/>
                                 </div>
                             </div>
                             
                             <div class='form-group'>
                                 <label for='birthday'>birthday</label>
-                                <input type='date' class='form-control' name='birthday' value='{{ $patient->birthday }}'/>
+                                <input type='date' class='form-control' name='birthday' value="{{ old('birthday', $patient->birthday) }}"/>
                             </div>
 
                             <input type='submit' class='form-control' />
