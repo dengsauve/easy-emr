b29f3b52e23ebcdd977ba77c802a48ed2b71198a
Dennis Sauve <dengsauve@yahoo.com>
2019-04-22 21:03:47 -0700

    Show Appointment

---
+++ resources/views/appointment/show.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">
+                    {{ $appointment->patient->name() . ' with ' . $appointment->user->name }}
+                </div>
+
+                <div class="card-body">
+                    <dl class='dl-horizontal'>
+                        <dt>
+                            Date and Time
+                        </dt>
+                        <dd>
+                            {{ $appointment->appointment_start_time }}
+                        </dd>
+                        <dt>
+                            Duration
+                        </dt>
+                        <dd>
+                            {{ $appointment->appointment_duration }} minutes
+                        </dd>
+                        <dt>
+                            Notes
+                        </dt>
+                        <dd>
+                            {{ $appointment->notes }}
+                        </dd>
+                    </dl>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
