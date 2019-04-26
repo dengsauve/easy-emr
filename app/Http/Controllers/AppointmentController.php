469f7227528639dd2ffb903ebb6d009cf725f61f
Dennis Sauve <dengsauve@yahoo.com>
2019-04-25 17:54:47 -0700

    Force auth on Appointment Routes

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -8,6 +8,12 @@ use App\Http\Requests\StoreAppointment;
 
 class AppointmentController extends Controller
 {
+    // Any user seeing Patients needs to be authorized
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     /**
      * Display a listing of the resource.
      *
